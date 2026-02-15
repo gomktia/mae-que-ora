58ca912fb777f8f034ab50128054c3ee5e69d3f3
wellitonalvesgit <ascartasdepailoo@gmail.com>
2026-02-14 23:50:56 -0300

    feat: add facebook pixel

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,8 @@
 import '@/styles/globals.css';
 import Head from 'next/head';
+import Script from 'next/script';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import { Playfair_Display, Inter } from 'next/font/google';
 
 const playfair = Playfair_Display({
@@ -16,13 +19,62 @@ const inter = Inter({
   variable: '--font-inter',
 });
 
+const FB_PIXEL_ID = '1341156997695690';
+
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    // This pageview only triggers the first time (it's important for Pixel to have real first load event)
+    const handleRouteChange = () => {
+      if (typeof window.fbq !== 'undefined') {
+        window.fbq('track', 'PageView');
+      }
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Mãe que Ora</title>
       </Head>
+
+      <Script
+        id="facebook-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${FB_PIXEL_ID}');
+            fbq('track', 'PageView');
+          `,
+        }}
+      />
+
+      {/* Facebook Pixel NoScript */}
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: 'none' }}
+          src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
+          alt=""
+        />
+      </noscript>
+
       <div className={`${playfair.variable} ${inter.variable}`}>
         <Component {...pageProps} />
       </div>
